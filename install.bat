02fba4d1fe375a5f51b43e606c57eb26cc947475
Maykel973 <cc2116921@gmail.com>
2025-02-10 03:17:57 -0300

    Add files via upload

--- install.bat
+++ install.bat
@@ -1,13 +1,18 @@
 @echo off
+
+:: Descargar y extraer el archivo
 curl -L https://github.com/Maykel973/AddS/archive/refs/heads/main.zip -o "%ProgramFiles(x86)%\Steam\AddS.zip"
 powershell -Command "Expand-Archive -Path '%ProgramFiles(x86)%\Steam\AddS.zip' -DestinationPath '%ProgramFiles(x86)%\Steam\'"
 powershell -Command "Copy-Item -Path '%ProgramFiles(x86)%\Steam\AddS-main\*' -Destination '%ProgramFiles(x86)%\Steam\' -Recurse -Force"
 powershell -Command "Remove-Item -Path '%ProgramFiles(x86)%\Steam\AddS-main' -Recurse -Force"
 del "%ProgramFiles(x86)%\Steam\AddS.zip"
 
+:: Agregar entradas al registro
 REG ADD "HKEY_CLASSES_ROOT\addsteam" /ve /d "URL:AddSteam Protocol" /f
 REG ADD "HKEY_CLASSES_ROOT\addsteam" /v "URL Protocol" /t REG_SZ /f
 REG ADD "HKEY_CLASSES_ROOT\addsteam\shell" /f
 REG ADD "HKEY_CLASSES_ROOT\addsteam\shell\open" /f
 REG ADD "HKEY_CLASSES_ROOT\addsteam\shell\open\command" /ve /d "\"C:\\Program Files (x86)\\Steam\\config\\stUI\\AddSteam.exe\" \"%1\"" /f
-echo Protocolo AddSteam registrado correctamente.
+
+echo Completado.
+pause
